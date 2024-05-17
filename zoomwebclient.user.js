90efe0b42f1f3de8d072474f87220230d0f12ca3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-17 16:03:59 +0900

    Update zoomwebclient.user.js

--- zoomwebclient.user.js
+++ zoomwebclient.user.js
@@ -1,13 +1,11 @@
 // ==UserScript==
 // @name        Zoom.us Web Client
 // @namespace   masaya.taniguchi
-// @match       https://*.zoom.us/*
+// @match       https://*.zoom.us/j/*
 // @grant       none
 // @version     1.0
 // @author      Masaya Taniguchi
 // @description 5/17/2024, 3:14:53 PM
 // ==/UserScript==
 
-if (window.location.href.indexOf(".us/j/") > 0) {
-  window.location.href = window.location.href.replace(".us/j/", ".us/wc/join/")
-}
+window.location.href = window.location.href.replace(".us/j/", ".us/wc/join/")
