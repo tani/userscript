f3432b9d210c730708fa2a5377e0675dbde2b278
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-17 16:01:21 +0900

    Create oldreddit.user.js

---
+++ oldreddit.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        Reddit Classic
+// @namespace   Violentmonkey Scripts
+// @match       https://www.reddit.com/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 5/17/2024, 3:59:43 PM
+// ==/UserScript==
+
+window.location.href = window.location.href.replace("www.reddit.com/", "old.reddit.com/")
