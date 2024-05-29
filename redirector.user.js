0eac819852e6a56aa342fed0fc87629c356df074
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 15:32:06 +0900

    Update redirector.user.js

--- redirector.user.js
+++ redirector.user.js
@@ -4,9 +4,8 @@
 // @match       https://www.reddit.com/*
 // @match       https://*.zoom.us/j/*
 // @match       https://*.zoom.us/s/*
-// @match       https://*.slack.com/archives/*
 // @grant       none
-// @version     1.3
+// @version     1.4
 // @author      Masaya Taniguchi
 // @description 5/17/2024, 3:59:43 PM
 // @run-at      document-start
@@ -21,4 +20,3 @@ function redirect(from, to) {
 redirect("www.reddit.com/", "old.reddit.com/")
 redirect("zoom.us/j/", "zoom.us/wc/join/")
 redirect("zoom.us/s/", "zoom.us/wc/join/")
-redirect("slack.com/archives/", "slack.com/messages/")
